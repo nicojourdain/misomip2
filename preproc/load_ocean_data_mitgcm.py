correct grid angle calculation

preproc/load_ocean_data_mitgcm.py
@@ -102,19 +102,16 @@ def load_oce_mod_mitgcm(files_T='MITgcm_all.nc',\
    domain_maxlon = lonT.max().values
 
    # grid mesh widths along x and y at C/T, U and V points [m]:
-   dxT = xr.DataArray( 0.500000000 * (ncM.dxC.values+ncM.dxC.shift(XG=-1).values), dims=['YC', 'XC'] )
-   dyT = xr.DataArray( 0.500000000 * (ncM.dyC.values+ncM.dyC.shift(YG=-1).values), dims=['YC', 'XC'] )
+   dxT = xr.DataArray( 0.500000000 * (ncM.dxC.values+ncM.dxC.shift(XG=1).values), dims=['YC', 'XC'] )
+   dyT = xr.DataArray( 0.500000000 * (ncM.dyC.values+ncM.dyC.shift(YG=1).values), dims=['YC', 'XC'] )
 
-   dlatTdx = 0.500000000 * ( latT.shift(XC=1) - latT.shift(XC=-1) )
-   dlatUdx = 0.500000000 * ( latU.shift(XG=1) - latU.shift(XG=-1) )
-   dlatVdx = 0.500000000 * ( latV.shift(XC=1) - latV.shift(XC=-1) )
+   dlatTdx = 0.500000000 * ( latT.shift(XC=-1) - latT.shift(XC=1) )
+   dlatUdx = 0.500000000 * ( latU.shift(XG=-1) - latU.shift(XG=1) )
+   dlatVdx = 0.500000000 * ( latV.shift(XC=-1) - latV.shift(XC=1) )
 
-   dlonTdx = 0.500000000 * ( lonT.shift(XC=1) - lonT.shift(XC=-1) ) 
-   dlonUdx = 0.500000000 * ( lonU.shift(XG=1) - lonU.shift(XG=-1) ) 
-   dlonVdx = 0.500000000 * ( lonV.shift(XC=1) - lonV.shift(XC=-1) )
-
-   # depth of U, V, C/T grids (neglecting the effects of partial steps in the interpolation) [m, positive in the ocean]
-   depTUV=ncM.Z*(-1) 
+   dlonTdx = 0.500000000 * ( lonT.shift(XC=-1) - lonT.shift(XC=1) ) 
+   dlonUdx = 0.500000000 * ( lonU.shift(XG=-1) - lonU.shift(XG=1) ) 
+   dlonVdx = 0.500000000 * ( lonV.shift(XC=-1) - lonV.shift(XC=1) )
 
    # local C/T, U, V grid rotation angle compared to the (zonal,meridional) direction [rad]
    thetaT = np.arctan2( dlatTdx, dlonTdx*np.cos(latT*np.pi/180.) )
@@ -122,6 +119,9 @@ def load_oce_mod_mitgcm(files_T='MITgcm_all.nc',\
    thetaV = np.arctan2( dlatVdx, dlonVdx*np.cos(latV*np.pi/180.) )
    print('    Minimum local grid angle in degrees w.r.t. (zonal,meridional):',thetaU.min().values*180./np.pi)
    print('    Maximum local grid angle in degrees w.r.t. (zonal,meridional):',thetaU.max().values*180./np.pi)
+
+   # depth of U, V, C/T grids (neglecting the effects of partial steps in the interpolation) [m, positive in the ocean]
+   depTUV=ncM.Z*(-1)
 
    [mz, my, mx] = ncM.hFacC.shape
 